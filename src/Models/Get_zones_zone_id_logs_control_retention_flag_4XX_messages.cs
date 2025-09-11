ecc69b91008c68dbcd847fb83f492eafd8377fed
Jake Soenneker <jake@soeneker.com>
2025-09-11 17:13:40 +0000

    Automated update

--- src/Models/Get_zones_zone_id_logs_control_retention_flag_4XX_messages.cs
+++ src/Models/Get_zones_zone_id_logs_control_retention_flag_4XX_messages.cs
@@ -9,26 +9,18 @@ namespace Soenneker.Cloudflare.OpenApiClient.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class Workers_script_response_upload_single : global::Soenneker.Cloudflare.OpenApiClient.Models.Workers_api_response_common, IParsable
+    public partial class Get_zones_zone_id_logs_control_retention_flag_4XX_messages : global::Soenneker.Cloudflare.OpenApiClient.Models.Logcontrol_messages_Wrapper, IParsable
     #pragma warning restore CS1591
     {
-        /// <summary>The result property</summary>
-#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
-#nullable enable
-        public global::Soenneker.Cloudflare.OpenApiClient.Models.Workers_script_response_upload? Result { get; set; }
-#nullable restore
-#else
-        public global::Soenneker.Cloudflare.OpenApiClient.Models.Workers_script_response_upload Result { get; set; }
-#endif
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Workers_script_response_upload_single"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Get_zones_zone_id_logs_control_retention_flag_4XX_messages"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static new global::Soenneker.Cloudflare.OpenApiClient.Models.Workers_script_response_upload_single CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static new global::Soenneker.Cloudflare.OpenApiClient.Models.Get_zones_zone_id_logs_control_retention_flag_4XX_messages CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             _ = parseNode ?? throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.Cloudflare.OpenApiClient.Models.Workers_script_response_upload_single();
+            return new global::Soenneker.Cloudflare.OpenApiClient.Models.Get_zones_zone_id_logs_control_retention_flag_4XX_messages();
         }
         /// <summary>
         /// The deserialization information for the current model
@@ -38,7 +30,6 @@ namespace Soenneker.Cloudflare.OpenApiClient.Models
         {
             return new Dictionary<string, Action<IParseNode>>(base.GetFieldDeserializers())
             {
-                { "result", n => { Result = n.GetObjectValue<global::Soenneker.Cloudflare.OpenApiClient.Models.Workers_script_response_upload>(global::Soenneker.Cloudflare.OpenApiClient.Models.Workers_script_response_upload.CreateFromDiscriminatorValue); } },
             };
         }
         /// <summary>
@@ -49,7 +40,6 @@ namespace Soenneker.Cloudflare.OpenApiClient.Models
         {
             _ = writer ?? throw new ArgumentNullException(nameof(writer));
             base.Serialize(writer);
-            writer.WriteObjectValue<global::Soenneker.Cloudflare.OpenApiClient.Models.Workers_script_response_upload>("result", Result);
         }
     }
 }
