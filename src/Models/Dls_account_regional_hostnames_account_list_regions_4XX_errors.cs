0ed6713f13148eff25bcdd1458b7ac77dcd304a9
Jake Soenneker <jake@soeneker.com>
2025-09-13 17:09:42 +0000

    Automated update

--- src/Models/Dls_account_regional_hostnames_account_list_regions_4XX_errors.cs
+++ src/Models/Dls_account_regional_hostnames_account_list_regions_4XX_errors.cs
@@ -9,18 +9,18 @@ namespace Soenneker.Cloudflare.OpenApiClient.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class Custom_indicator_feeds_get_indicator_feed_data_4XX_errors : global::Soenneker.Cloudflare.OpenApiClient.Models.Custom_indicator_feeds_messages_Wrapper, IParsable
+    public partial class Dls_account_regional_hostnames_account_list_regions_4XX_errors : global::Soenneker.Cloudflare.OpenApiClient.Models.Dls_messages_Wrapper, IParsable
     #pragma warning restore CS1591
     {
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Custom_indicator_feeds_get_indicator_feed_data_4XX_errors"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Dls_account_regional_hostnames_account_list_regions_4XX_errors"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static new global::Soenneker.Cloudflare.OpenApiClient.Models.Custom_indicator_feeds_get_indicator_feed_data_4XX_errors CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static new global::Soenneker.Cloudflare.OpenApiClient.Models.Dls_account_regional_hostnames_account_list_regions_4XX_errors CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             _ = parseNode ?? throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.Cloudflare.OpenApiClient.Models.Custom_indicator_feeds_get_indicator_feed_data_4XX_errors();
+            return new global::Soenneker.Cloudflare.OpenApiClient.Models.Dls_account_regional_hostnames_account_list_regions_4XX_errors();
         }
         /// <summary>
         /// The deserialization information for the current model
