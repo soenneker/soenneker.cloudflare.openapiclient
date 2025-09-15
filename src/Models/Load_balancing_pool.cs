fe876e753841ba1a7725f7c8dbd205ab4d573298
Jake Soenneker <jake@soeneker.com>
2025-09-15 17:22:50 +0000

    Automated update

--- src/Models/Load_balancing_pool.cs
+++ src/Models/Load_balancing_pool.cs
@@ -17,10 +17,10 @@ namespace Soenneker.Cloudflare.OpenApiClient.Models
         /// <summary>A list of regions from which to run health checks. Null means every Cloudflare data center.</summary>
 #if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
 #nullable enable
-        public List<global::Soenneker.Cloudflare.OpenApiClient.Models.Monitors?>? CheckRegions { get; set; }
+        public List<global::Soenneker.Cloudflare.OpenApiClient.Models.Pools?>? CheckRegions { get; set; }
 #nullable restore
 #else
-        public List<global::Soenneker.Cloudflare.OpenApiClient.Models.Monitors?> CheckRegions { get; set; }
+        public List<global::Soenneker.Cloudflare.OpenApiClient.Models.Pools?> CheckRegions { get; set; }
 #endif
         /// <summary>The created_on property</summary>
         public DateTimeOffset? CreatedOn { get; set; }
@@ -141,7 +141,7 @@ namespace Soenneker.Cloudflare.OpenApiClient.Models
         {
             return new Dictionary<string, Action<IParseNode>>
             {
-                { "check_regions", n => { CheckRegions = n.GetCollectionOfEnumValues<global::Soenneker.Cloudflare.OpenApiClient.Models.Monitors>()?.AsList(); } },
+                { "check_regions", n => { CheckRegions = n.GetCollectionOfEnumValues<global::Soenneker.Cloudflare.OpenApiClient.Models.Pools>()?.AsList(); } },
                 { "created_on", n => { CreatedOn = n.GetDateTimeOffsetValue(); } },
                 { "description", n => { Description = n.GetStringValue(); } },
                 { "disabled_at", n => { DisabledAt = n.GetDateTimeOffsetValue(); } },
@@ -168,7 +168,7 @@ namespace Soenneker.Cloudflare.OpenApiClient.Models
         public virtual void Serialize(ISerializationWriter writer)
         {
             _ = writer ?? throw new ArgumentNullException(nameof(writer));
-            writer.WriteCollectionOfEnumValues<global::Soenneker.Cloudflare.OpenApiClient.Models.Monitors>("check_regions", CheckRegions);
+            writer.WriteCollectionOfEnumValues<global::Soenneker.Cloudflare.OpenApiClient.Models.Pools>("check_regions", CheckRegions);
             writer.WriteDateTimeOffsetValue("created_on", CreatedOn);
             writer.WriteStringValue("description", Description);
             writer.WriteDateTimeOffsetValue("disabled_at", DisabledAt);
