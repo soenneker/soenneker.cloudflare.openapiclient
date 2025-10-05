d13b67479c940c03088a3ccae19296616c151dfb
Jake Soenneker <jake@soeneker.com>
2025-10-05 17:13:56 +0000

    Automated update

--- src/Models/Client_certificate_for_a_zone_list_hostname_associations_4XX_messages.cs
+++ src/Models/Client_certificate_for_a_zone_list_hostname_associations_4XX_messages.cs
@@ -9,35 +9,26 @@ namespace Soenneker.Cloudflare.OpenApiClient.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class Zones_0_hold_delete_4XX_result : IAdditionalDataHolder, IParsable
+    public partial class Client_certificate_for_a_zone_list_hostname_associations_4XX_messages : global::Soenneker.Cloudflare.OpenApiClient.Models.Tls_certificates_and_hostnames_messages_Wrapper, IParsable
     #pragma warning restore CS1591
     {
-        /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
-        public IDictionary<string, object> AdditionalData { get; set; }
-        /// <summary>
-        /// Instantiates a new <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Zones_0_hold_delete_4XX_result"/> and sets the default values.
-        /// </summary>
-        public Zones_0_hold_delete_4XX_result()
-        {
-            AdditionalData = new Dictionary<string, object>();
-        }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Zones_0_hold_delete_4XX_result"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Client_certificate_for_a_zone_list_hostname_associations_4XX_messages"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static global::Soenneker.Cloudflare.OpenApiClient.Models.Zones_0_hold_delete_4XX_result CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static new global::Soenneker.Cloudflare.OpenApiClient.Models.Client_certificate_for_a_zone_list_hostname_associations_4XX_messages CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             _ = parseNode ?? throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.Cloudflare.OpenApiClient.Models.Zones_0_hold_delete_4XX_result();
+            return new global::Soenneker.Cloudflare.OpenApiClient.Models.Client_certificate_for_a_zone_list_hostname_associations_4XX_messages();
         }
         /// <summary>
         /// The deserialization information for the current model
         /// </summary>
         /// <returns>A IDictionary&lt;string, Action&lt;IParseNode&gt;&gt;</returns>
-        public virtual IDictionary<string, Action<IParseNode>> GetFieldDeserializers()
+        public override IDictionary<string, Action<IParseNode>> GetFieldDeserializers()
         {
-            return new Dictionary<string, Action<IParseNode>>
+            return new Dictionary<string, Action<IParseNode>>(base.GetFieldDeserializers())
             {
             };
         }
@@ -45,10 +36,10 @@ namespace Soenneker.Cloudflare.OpenApiClient.Models
         /// Serializes information the current object
         /// </summary>
         /// <param name="writer">Serialization writer to use to serialize this model</param>
-        public virtual void Serialize(ISerializationWriter writer)
+        public override void Serialize(ISerializationWriter writer)
         {
             _ = writer ?? throw new ArgumentNullException(nameof(writer));
-            writer.WriteAdditionalData(AdditionalData);
+            base.Serialize(writer);
         }
     }
 }
