807271f8715a11ed76746b3846f65becaeb11eef
Jake Soenneker <jake@soeneker.com>
2025-10-14 17:15:34 +0000

    Automated update

--- src/Models/Notification_policies_list_notification_policies_4XX_errors.cs
+++ src/Models/Notification_policies_list_notification_policies_4XX_errors.cs
@@ -9,18 +9,18 @@ namespace Soenneker.Cloudflare.OpenApiClient.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class Get_zones_zone_id_logpush_datasets_dataset_id_fields_4XX_messages : global::Soenneker.Cloudflare.OpenApiClient.Models.Logpush_messages_Wrapper, IParsable
+    public partial class Notification_policies_list_notification_policies_4XX_errors : global::Soenneker.Cloudflare.OpenApiClient.Models.Aaa_components_schemas_messages_Wrapper, IParsable
     #pragma warning restore CS1591
     {
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Get_zones_zone_id_logpush_datasets_dataset_id_fields_4XX_messages"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Notification_policies_list_notification_policies_4XX_errors"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static new global::Soenneker.Cloudflare.OpenApiClient.Models.Get_zones_zone_id_logpush_datasets_dataset_id_fields_4XX_messages CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static new global::Soenneker.Cloudflare.OpenApiClient.Models.Notification_policies_list_notification_policies_4XX_errors CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             _ = parseNode ?? throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.Cloudflare.OpenApiClient.Models.Get_zones_zone_id_logpush_datasets_dataset_id_fields_4XX_messages();
+            return new global::Soenneker.Cloudflare.OpenApiClient.Models.Notification_policies_list_notification_policies_4XX_errors();
         }
         /// <summary>
         /// The deserialization information for the current model
