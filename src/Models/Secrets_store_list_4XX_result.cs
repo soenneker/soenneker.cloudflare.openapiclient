4423d63a4514455fd24875e42e6a57e1a07e1779
Jake Soenneker <jake@soeneker.com>
2025-10-19 17:14:18 +0000

    Automated update

--- src/Models/Secrets_store_list_4XX_result.cs
+++ src/Models/Secrets_store_list_4XX_result.cs
@@ -9,26 +9,35 @@ namespace Soenneker.Cloudflare.OpenApiClient.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class Waf_content_scanning_enable_4XX_messages : global::Soenneker.Cloudflare.OpenApiClient.Models.Waf_product_api_bundle_messages_Wrapper, IParsable
+    public partial class Secrets_store_list_4XX_result : IAdditionalDataHolder, IParsable
     #pragma warning restore CS1591
     {
+        /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
+        public IDictionary<string, object> AdditionalData { get; set; }
+        /// <summary>
+        /// Instantiates a new <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Secrets_store_list_4XX_result"/> and sets the default values.
+        /// </summary>
+        public Secrets_store_list_4XX_result()
+        {
+            AdditionalData = new Dictionary<string, object>();
+        }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Waf_content_scanning_enable_4XX_messages"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Secrets_store_list_4XX_result"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static new global::Soenneker.Cloudflare.OpenApiClient.Models.Waf_content_scanning_enable_4XX_messages CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static global::Soenneker.Cloudflare.OpenApiClient.Models.Secrets_store_list_4XX_result CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             _ = parseNode ?? throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.Cloudflare.OpenApiClient.Models.Waf_content_scanning_enable_4XX_messages();
+            return new global::Soenneker.Cloudflare.OpenApiClient.Models.Secrets_store_list_4XX_result();
         }
         /// <summary>
         /// The deserialization information for the current model
         /// </summary>
         /// <returns>A IDictionary&lt;string, Action&lt;IParseNode&gt;&gt;</returns>
-        public override IDictionary<string, Action<IParseNode>> GetFieldDeserializers()
+        public virtual IDictionary<string, Action<IParseNode>> GetFieldDeserializers()
         {
-            return new Dictionary<string, Action<IParseNode>>(base.GetFieldDeserializers())
+            return new Dictionary<string, Action<IParseNode>>
             {
             };
         }
@@ -36,10 +45,10 @@ namespace Soenneker.Cloudflare.OpenApiClient.Models
         /// Serializes information the current object
         /// </summary>
         /// <param name="writer">Serialization writer to use to serialize this model</param>
-        public override void Serialize(ISerializationWriter writer)
+        public virtual void Serialize(ISerializationWriter writer)
         {
             _ = writer ?? throw new ArgumentNullException(nameof(writer));
-            base.Serialize(writer);
+            writer.WriteAdditionalData(AdditionalData);
         }
     }
 }
