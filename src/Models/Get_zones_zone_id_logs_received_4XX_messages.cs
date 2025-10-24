ca9635acd29cc9fc0795869ba38245a81f23bdd6
Jake Soenneker <jake@soeneker.com>
2025-10-24 17:15:27 +0000

    Automated update

--- src/Models/Get_zones_zone_id_logs_received_4XX_messages.cs
+++ src/Models/Get_zones_zone_id_logs_received_4XX_messages.cs
@@ -9,18 +9,18 @@ namespace Soenneker.Cloudflare.OpenApiClient.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class Iam_schemas_account : global::Soenneker.Cloudflare.OpenApiClient.Models.Iam_account, IParsable
+    public partial class Get_zones_zone_id_logs_received_4XX_messages : global::Soenneker.Cloudflare.OpenApiClient.Models.Logshare_messages_Wrapper, IParsable
     #pragma warning restore CS1591
     {
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Iam_schemas_account"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Get_zones_zone_id_logs_received_4XX_messages"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static new global::Soenneker.Cloudflare.OpenApiClient.Models.Iam_schemas_account CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static new global::Soenneker.Cloudflare.OpenApiClient.Models.Get_zones_zone_id_logs_received_4XX_messages CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             _ = parseNode ?? throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.Cloudflare.OpenApiClient.Models.Iam_schemas_account();
+            return new global::Soenneker.Cloudflare.OpenApiClient.Models.Get_zones_zone_id_logs_received_4XX_messages();
         }
         /// <summary>
         /// The deserialization information for the current model
