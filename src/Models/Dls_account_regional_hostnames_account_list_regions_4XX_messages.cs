877cc381cbad0fe86ed98f9ac15f10a6f1bc6f43
Jake Soenneker <jake@soeneker.com>
2025-11-19 17:19:02 +0000

    Automated update

--- src/Models/Dls_account_regional_hostnames_account_list_regions_4XX_messages.cs
+++ src/Models/Dls_account_regional_hostnames_account_list_regions_4XX_messages.cs
@@ -9,18 +9,18 @@ namespace Soenneker.Cloudflare.OpenApiClient.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class Logcontrol_api_response_single : global::Soenneker.Cloudflare.OpenApiClient.Models.Logcontrol_api_response_common, IParsable
+    public partial class Dls_account_regional_hostnames_account_list_regions_4XX_messages : global::Soenneker.Cloudflare.OpenApiClient.Models.Dls_messages_Wrapper, IParsable
     #pragma warning restore CS1591
     {
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Logcontrol_api_response_single"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Dls_account_regional_hostnames_account_list_regions_4XX_messages"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static new global::Soenneker.Cloudflare.OpenApiClient.Models.Logcontrol_api_response_single CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static new global::Soenneker.Cloudflare.OpenApiClient.Models.Dls_account_regional_hostnames_account_list_regions_4XX_messages CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.Cloudflare.OpenApiClient.Models.Logcontrol_api_response_single();
+            return new global::Soenneker.Cloudflare.OpenApiClient.Models.Dls_account_regional_hostnames_account_list_regions_4XX_messages();
         }
         /// <summary>
         /// The deserialization information for the current model
