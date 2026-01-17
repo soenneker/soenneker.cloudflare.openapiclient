9dd5e2320ff6eb0d895d59e7402e6c5592f9edbb
Jake Soenneker <jake@soeneker.com>
2026-01-17 17:14:57 +0000

    Automated update

--- src/Models/Iam_schemas_account_managed_by.cs
+++ src/Models/Iam_schemas_account_managed_by.cs
@@ -7,29 +7,46 @@ using System.IO;
 using System;
 namespace Soenneker.Cloudflare.OpenApiClient.Models
 {
+    /// <summary>
+    /// Parent container details
+    /// </summary>
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
-    #pragma warning disable CS1591
-    public partial class Post_zones_zone_id_logpush_ownership_4XX_result : IAdditionalDataHolder, IParsable
-    #pragma warning restore CS1591
+    public partial class Iam_schemas_account_managed_by : IAdditionalDataHolder, IParsable
     {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
+        /// <summary>ID of the parent Organization, if one exists</summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+        public string? ParentOrgId { get; private set; }
+#nullable restore
+#else
+        public string ParentOrgId { get; private set; }
+#endif
+        /// <summary>Name of the parent Organization, if one exists</summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+        public string? ParentOrgName { get; private set; }
+#nullable restore
+#else
+        public string ParentOrgName { get; private set; }
+#endif
         /// <summary>
-        /// Instantiates a new <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Post_zones_zone_id_logpush_ownership_4XX_result"/> and sets the default values.
+        /// Instantiates a new <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Iam_schemas_account_managed_by"/> and sets the default values.
         /// </summary>
-        public Post_zones_zone_id_logpush_ownership_4XX_result()
+        public Iam_schemas_account_managed_by()
         {
             AdditionalData = new Dictionary<string, object>();
         }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Post_zones_zone_id_logpush_ownership_4XX_result"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Iam_schemas_account_managed_by"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static global::Soenneker.Cloudflare.OpenApiClient.Models.Post_zones_zone_id_logpush_ownership_4XX_result CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static global::Soenneker.Cloudflare.OpenApiClient.Models.Iam_schemas_account_managed_by CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.Cloudflare.OpenApiClient.Models.Post_zones_zone_id_logpush_ownership_4XX_result();
+            return new global::Soenneker.Cloudflare.OpenApiClient.Models.Iam_schemas_account_managed_by();
         }
         /// <summary>
         /// The deserialization information for the current model
@@ -39,6 +56,8 @@ namespace Soenneker.Cloudflare.OpenApiClient.Models
         {
             return new Dictionary<string, Action<IParseNode>>
             {
+                { "parent_org_id", n => { ParentOrgId = n.GetStringValue(); } },
+                { "parent_org_name", n => { ParentOrgName = n.GetStringValue(); } },
             };
         }
         /// <summary>
