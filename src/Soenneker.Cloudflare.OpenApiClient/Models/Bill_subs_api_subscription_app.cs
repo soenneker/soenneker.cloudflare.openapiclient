d33d3892398022cbea9c8027af93058f548622b0
Jake Soenneker <jake@soeneker.com>
2026-03-31 05:58:51 +0000

    Automated update

--- src/Soenneker.Cloudflare.OpenApiClient/Models/Bill_subs_api_subscription_app.cs
+++ src/Soenneker.Cloudflare.OpenApiClient/Models/Bill_subs_api_subscription_app.cs
@@ -9,27 +9,35 @@ namespace Soenneker.Cloudflare.OpenApiClient.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class Waf_content_scanning_disable_4XX_result : IAdditionalDataHolder, IParsable
+    public partial class Bill_subs_api_subscription_app : IAdditionalDataHolder, IParsable
     #pragma warning restore CS1591
     {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
+        /// <summary>app install id.</summary>
+#if NETSTANDARD2_1_OR_GREATER || NETCOREAPP3_1_OR_GREATER
+#nullable enable
+        public string? InstallId { get; set; }
+#nullable restore
+#else
+        public string InstallId { get; set; }
+#endif
         /// <summary>
-        /// Instantiates a new <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Waf_content_scanning_disable_4XX_result"/> and sets the default values.
+        /// Instantiates a new <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Bill_subs_api_subscription_app"/> and sets the default values.
         /// </summary>
-        public Waf_content_scanning_disable_4XX_result()
+        public Bill_subs_api_subscription_app()
         {
             AdditionalData = new Dictionary<string, object>();
         }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Waf_content_scanning_disable_4XX_result"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Cloudflare.OpenApiClient.Models.Bill_subs_api_subscription_app"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static global::Soenneker.Cloudflare.OpenApiClient.Models.Waf_content_scanning_disable_4XX_result CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static global::Soenneker.Cloudflare.OpenApiClient.Models.Bill_subs_api_subscription_app CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.Cloudflare.OpenApiClient.Models.Waf_content_scanning_disable_4XX_result();
+            return new global::Soenneker.Cloudflare.OpenApiClient.Models.Bill_subs_api_subscription_app();
         }
         /// <summary>
         /// The deserialization information for the current model
@@ -39,6 +47,7 @@ namespace Soenneker.Cloudflare.OpenApiClient.Models
         {
             return new Dictionary<string, Action<IParseNode>>
             {
+                { "install_id", n => { InstallId = n.GetStringValue(); } },
             };
         }
         /// <summary>
@@ -48,6 +57,7 @@ namespace Soenneker.Cloudflare.OpenApiClient.Models
         public virtual void Serialize(ISerializationWriter writer)
         {
             if(ReferenceEquals(writer, null)) throw new ArgumentNullException(nameof(writer));
+            writer.WriteStringValue("install_id", InstallId);
             writer.WriteAdditionalData(AdditionalData);
         }
     }
